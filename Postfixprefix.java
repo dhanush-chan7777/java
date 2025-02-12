9aa07fbd9825596c8efe97d65645336e09611311
dhanush-chan7777 <148308880+dhanush-chan7777@users.noreply.github.com>
2025-02-12 14:48:17 +0530

    Create Postfixprefix.java

---
+++ Postfixprefix.java
@@ -0,0 +1,13 @@
+public class Postfixprefix
+{
+	public static void main(String[] args) {
+
+     int x=10;
+     int v= --x + x++ + ++x + --x + x++;
+     System.out.println(v);
+     
+
+
+
+	}
+}
