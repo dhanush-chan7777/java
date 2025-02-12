14dac0c1da60035dd5ed050d38ce9be1f55360b8
dhanush-chan7777 <148308880+dhanush-chan7777@users.noreply.github.com>
2025-02-12 12:41:25 +0530

    Create Bitwise.java

---
+++ Bitwise.java
@@ -0,0 +1,12 @@
+class Bitwise{
+    public static void main(String[] args){
+        int a=492;
+        int b=333;
+        
+      System.out.println("Bitwise OR:"+(a|b));
+      System.out.println("Bitwise AND:"+(a&b));
+      System.out.println("Bitwise XOR:"+(a^b));
+
+ 
+    }
+}
