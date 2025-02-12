cb8bc8ac60b183fc41b7128c72da14e8dcdff9c0
dhanush-chan7777 <148308880+dhanush-chan7777@users.noreply.github.com>
2025-02-12 12:02:54 +0530

    Create Arithmetic.java

---
+++ Arithmetic.java
@@ -0,0 +1,14 @@
+ public class Arithmetic{
+    public static void main(String[] args){
+       // System.out.println("Hello World");
+      byte a=2;
+      byte b=124;
+      byte c=(byte)(a*b);
+    System.out.println(a+b);
+    System.out.println(b-a);
+    System.out.println(c);
+   System.out.println(b/a);
+
+
+    }
+}
