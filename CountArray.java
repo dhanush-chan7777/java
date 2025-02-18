15a6cd6d89acffb63e268dcfd45719b0d22fd7ce
dhanush-chan7777 <148308880+dhanush-chan7777@users.noreply.github.com>
2025-02-18 14:56:20 +0530

    Update CountArray.java

--- CountArray.java
+++ CountArray.java
@@ -1,14 +1,20 @@
 public class CountArray{
     public static void main(String []args){
         int count=0;
-        int a[]={1,1,0,1,1,0};
-        int b[]={1,1,0,1,1,0};
+        int notmatched=0;
+        int a[]={1,1,0,1,1,0,8};
+        int b[]={1,1,0,1,8,0};
         
-        for(int i=0;i<a.length;i++){
+        int n=Math.min(a.length,b.length);
+        for(int i=0;i<n;i++){
             if(a[i]==b[i]){
                 count++;
             }
+            else{
+                notmatched++;
+            }
         }
-        System.out.print("COUNT::"+count);
+        System.out.println("Matched::"+count);
+        System.out.println("Notmatched::"+notmatched);
     }
 }
