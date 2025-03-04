d1e3db5451d9a82ebf6de0c13530783b7df90f3c
dhanush-chan7777 <148308880+dhanush-chan7777@users.noreply.github.com>
2025-03-04 15:09:33 +0530

    Create SubstringBuildin.java

---
+++ SubstringBuildin.java
@@ -0,0 +1,7 @@
+public class SubstringBuildin{
+    public static void main(String []args){
+        String s="String is String";
+        String sub1=s.substring(10,11);
+        System.out.print(sub1);
+    }
+}
